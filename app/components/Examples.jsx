42bb472a8ca7f45536c3d47c72cb68a9a840bdae
AZR <azr@ip-192-169-217-33.ip.secureserver.net>
2017-12-08 00:09:25 +0530

    Update Example page fix

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
 var Examples = (props) => {
   return(
-    <h2>Examples Component</h2>
-    <p>Welcome to Examples page</p>
+    <div>
+      <h2>Examples Component</h2>
+      <p>Welcome to Examples page</p>
+    </div>
   );
 };
 
