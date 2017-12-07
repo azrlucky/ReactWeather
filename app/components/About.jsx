8685215fdf50ff64f3c5d0fb78f134efb7f7a793
AZR <azr@ip-192-169-217-33.ip.secureserver.net>
2017-12-08 00:11:08 +0530

    Update About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,7 +1,10 @@
 var React = require('react');
 var About = (props) => {
   return(
-    <h2>About Component</h2>
+    <div>
+      <h2>About Component</h2>
+      <p>Welcome to About page</p>
+    </div>
   );
 };
 
